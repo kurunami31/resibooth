1fdb389d97d70c3a7cc282dad3a98191989c7ee2
Christopher <user@resibooth.app>
2026-06-01 01:29:47 +0800

    Fix catch-all route for Express 5

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(resolve(__dirname, 'dist')))
 
-app.get('*', (_req, res) => {
+app.use((_req, res) => {
   res.sendFile(resolve(__dirname, 'dist', 'index.html'))
 })
 
