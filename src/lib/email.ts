fc39c8447b2ddc6b0ba97653f89617280a77fa69
Christopher <user@resibooth.app>
2026-06-01 04:26:54 +0800

    update email public key

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,5 +1,5 @@
 export const EMAIL_CONFIG = {
   serviceId: 'service_srda3wc',
-  templateId: 'template_gniwbfn',
-  publicKey: 'n4X2GrdluFxSMs6Oq',
+  templateId: 'template_9usq0cr',
+  publicKey: '2pmH8eb3NG7zzQggT',
 }
